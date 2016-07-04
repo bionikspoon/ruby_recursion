8608fa9a6e728d9a8601a3251cdb1f947c1cf260
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-04 01:53:10 -0500

    impliment fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,7 +1,17 @@
 def fibs n
+  return [] if n === 0
+  a, b = 1, 1
 
+  Enumerator.new do |y|
+    y << a
+    (n - 1).times do
+      y << b
+      a, b = b, a + b
+    end
+  end.to_a
 end
 
-def fibs_rec n
-
+def fibs_rec n, a: 1, b: 1
+  return [] if n === 0
+  [a] + fibs_rec(n-1, a: b, b: a + b)
 end
--- spec/lib/fibonacci_spec.rb
+++ spec/lib/fibonacci_spec.rb
@@ -5,10 +5,26 @@ describe '#fibs' do
   it 'should return 5 members' do
     fibs(5).should eq [1, 1, 2, 3, 5]
   end
+
+  it "should return empty array if n = 0" do
+    fibs(0).should eq []
+  end
+
+  it "should return 1 item" do
+    fibs(1).should eq [1]
+  end
 end
 
 describe "#fibs_rec" do
   it 'should return 5 members' do
     fibs_rec(5).should eq [1, 1, 2, 3, 5]
+  end
+
+  it "should return empty array if n = 0" do
+    fibs_rec(0).should eq []
+  end
+
+  it "should return 1 item" do
+    fibs_rec(1).should eq [1]
   end
 end
